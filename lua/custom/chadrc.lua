83418f6baac1b4cc50615793bacc04dcd4c9a775
Tobias Dummschat <dummschat@vilisto.de>
2023-12-01 12:09:39 +0100

    base custom config

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,8 @@
+-- custom/chadrc.lua meant to override that table in default_config.lua file
+
+local M = {}
+
+M.ui = {theme = "catppuccin"}
+M.plugins = "custom.plugins"
+
+return M
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,84 @@
+local plugins = {
+  -- Theme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+  },
+
+  -- You can use any plugin specification from lazy.nvim
+  {
+    "Pocco81/TrueZen.nvim",
+    cmd = { "TZAtaraxis", "TZMinimalist" },
+    config = function()
+      require "custom.configs.truezen" -- just an example path
+    end,
+  },
+
+  -- this opts will extend the default opts 
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults 
+        "vim",
+        "lua",
+
+        -- web dev 
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+        -- "vue", "svelte",
+
+       -- low level
+        "c",
+        "zig"
+      },
+    },
+  },
+
+  -- If your opts uses a function call, then make opts spec a function*
+  -- should return the modified default config as well
+  -- here we just call the default telescope config 
+  -- and then assign a function to some of its options
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function()
+      local conf = require "plugins.configs.telescope"
+      conf.defaults.mappings.i = {
+        ["<C-j>"] = require("telescope.actions").move_selection_next,
+        ["<Esc>"] = require("telescope.actions").close,
+      }
+
+      return conf
+    end,
+  },
+
+  -- In order to modify the `lspconfig` configuration:
+  {
+  "neovim/nvim-lspconfig",
+   config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+   end,
+  },
+
+   {
+   "williamboman/mason.nvim",
+   opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "html-lsp",
+        "prettier",
+        "stylua"
+      },
+    },
+  },
+}
+
+return plugins
+
